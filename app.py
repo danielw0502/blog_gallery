116f8fc737ca9e3b95f671df948b1e525ab02dba
Daniel Wang <wxd920502@gmail.com>
2018-11-30 21:28:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ from flask_uploads import UploadSet, configure_uploads, IMAGES
 # You may consider using a one-way hash to generate the password, and then
 # use the hash again in the login view to perform the comparison. This is just
 # for simplicity.
-ADMIN_PASSWORD = 'secret'
+ADMIN_PASSWORD = 'XXX'
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
 
 # The playhouse.flask_utils.FlaskDB object accepts database URL configuration.
